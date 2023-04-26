fb2c9a1c74d2ec930f87113cbdbd9563ee11f8c4
mjfarrelm <matthewfarrelm@googlemail.com>
2023-04-26 11:42:12 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 
-function App() {
+const App =() => {
+  const [INSERT CODE HERE, INSERT CODE HERE] = useState ([])
+
+  const fetchData = async () => {
+    const apiRequest = await fetch (" ")  /*INSERT API URL IN THE BRACKETS!*/ 
+    const apiData = await apiRequest.json()
+    
+    setpokeChar(apiData)
+
+    console.log(apiRequest)
+    console.log(apiData)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [] )
+
   return (
     <div>
+     
+    {INSERT.map((INSERT CODE)=>{
+      
+      return (
+        <div>
+        
+        </div>
+      )
+    })}
 
     </div>
   );
 }
 
 export default App;
+
+
+
+
