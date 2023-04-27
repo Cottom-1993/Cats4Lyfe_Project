49462c89cd6d642a1b6692ccfe25a0127f06731b
mjfarrelm <matthewfarrelm@googlemail.com>
2023-04-27 17:14:21 +0100

    carousel

--- src/App.js
+++ src/App.js
@@ -97,19 +97,20 @@ import './App.css';
 import  Modal  from './Modal';
 import { useState, useEffect } from 'react';
 
-// import {faker, animal, cat} from '@faker-js/faker';
+import {faker, animal, cat} from '@faker-js/faker';
 
 
-// const App =() => {
-//   const [catPics, setCatPics] = useState ([])
-//   // const catName = faker.animal.cat(catName);
-//   // const catBreed = faker.animal.cat(catBreed);
-//   // const catAge = faker.animal.cat(catAge);
+
+ 
+  const catName = faker.animal.cat(catName);
+  const catBreed = faker.animal.cat(catBreed);
+  const catAge = faker.animal.cat(catAge);
 
 
 const App = () => {
 const [catPics, setCatPics] = useState([]);
 const [showModal, setshowModal] = useState(false);
+const [currentIndex, setCurrentIndex] = useState(0);
 
 const fetchData = async () => {
   const apiRequest = await fetch ("https://api.thecatapi.com/v1/images/search?limit=10&api_key=enlRzO8WCB2SONijKb8GfoS6ehYsdi7v0g6QAgCpIvb8fPEybmyOPDHeK9704j0t");
@@ -134,19 +135,38 @@ const fetchData = async () => {
     }
   }, [] );
 
+  const carouselScrollRight = () => {
+    if (currentIndex === catPics.length-1){
+      setCurrentIndex(0)
+    }else{
+      setCurrentIndex(currentIndex +1)
+    }
+  }
+
+  const carouselScrollLeft = () => {
+    if (currentIndex === 0){
+      setCurrentIndex(catPics.length-1)
+    }else{
+      setCurrentIndex(currentIndex -1)
+    }
+  }
+
   return (
     <div>
-
+      <div id="catContainer">
     {catPics.map((singleCat, index) => {
       console.log(singleCat);
       return (
-        <div key={singleCat.id}>
+        <div className='catItem' key={singleCat.id} style={{transform:`translateX(-${currentIndex * 100}%)`}}>
           <img alt="cat for sale" src={singleCat.url} className={`cat-pic-${index}`} />
           <p>{singleCat.id}</p>
           <button className="openModal" onClick={()=>setshowModal(true)}>Add to basket</button>
         </div>
       );
       })}
+      </div>
+      <button onClick={carouselScrollRight}> &#8594; </button>
+      <button onClick={carouselScrollLeft}> &#8592; </button>
       <div id="ModalBanner">
           {showModal && <Modal showModal={showModal} />}
       </div>
